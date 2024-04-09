6e446d034d6aaf308d2aea2b4ca93b5c345760da
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2024-04-08 21:38:25 -0700

    initial commit for project2

---
+++ project2/README.md
@@ -0,0 +1,38 @@
+# Project 2
+
+## Building
+
+Command Line
+
+`make` to build. An executable called  `reservations` will be produced. 
+
+## Files
+
+`reservations.c`: The main code to launch the project
+
+## Data
+`seat_taken` - an array of seats
+
+## Functions
+
+`lock` : pthread_mutex_t lock initilizer
+
+* `reserve_seat()` : Function that takes n and reserves a seat in the seat_taken array
+    * `pthread_mutex_lock()` : Lock the mutex
+    * `is_free()` : Function that takes n and returns true is the seat is available
+    *  `pthread_mutex_unlock()` : Unlock the mutex
+
+* `free_seat()` : Function that takes n and frees a seat in the seat_taken array
+    * `pthread_mutex_lock()` : Lock the mutex
+    * `is_free` : Function that takes n and returns true is the seat is available
+    *  `pthread_mutex_unlock()` : Unlock the mutex
+
+* `verify_seat_count()` : Function that counts all the taken seats in the seat_taken array, compares the count and the seat_count global variable and returns true if they are the same. 
+
+* `*seat_broker()` : Function to buy and sell seats
+
+* `main()` : Main function that produces the threads, sets the seat_count, broker_count, transaction_count and seat_taken array. 
+Launches the threads and waits for them to complete. 
+
+## Notes
+    Used in class notes!
---
+++ project2/reservations.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// These will be initialized in main() from the command line.
+int seat_count;
+int broker_count;
+int *seat_taken;  // Array of seats
+int transaction_count;
+
+int seat_taken_count = 0;
+
+// Initialize the locks
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+int is_free(int n) {
+    // Returns true if the given seat is available.
+
+    if (seat_taken[n] == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    };
+}
+
+int reserve_seat(int n)
+{
+    // Attempt to reserve seat number n
+    if (n < 0 || n >= seat_count) {
+        return -1;
+    }
+
+    // lock the mutex for thread safety
+    pthread_mutex_lock(&lock);
+    // check if the seat is free
+    if (is_free(n)) {
+        seat_taken[n] = 1; // reserve the seat
+        seat_taken_count++; // increment the seat_taken_count
+        pthread_mutex_unlock(&lock); // unlock the mutex
+        return 0;
+    }
+    else {
+        pthread_mutex_unlock(&lock); // unlock the mutex
+        return -1;
+    }
+}
+
+int free_seat(int n)
+{
+    // Attempt to free (unreserve) seat number n
+
+    // check if the seat is within the bounds
+    if (n < 0 || n >= seat_count) {
+        return -1;
+    }
+
+    // lock the mutex for thread safety
+    pthread_mutex_lock(&lock);
+    // check if the seat is taken
+    if (!is_free(n)) {
+        seat_taken[n] = 0; // free the seat
+        // decrement the seat_taken_count if greater than 0
+        if (seat_taken_count > 0) {
+            seat_taken_count--;
+        }
+        pthread_mutex_unlock(&lock); // unlock the mutex
+        return 0;
+    }
+    else {
+        pthread_mutex_unlock(&lock); // unlock the mutex
+        return -1;
+    }
+}
+
+
+int verify_seat_count(void) {
+    // This function counts all the taken seats in the seat_taken[]
+    // array.
+    //
+    // It then compares the count with the seat_count global variable.
+    //
+    // It returns true if they are the same, false otherwise
+    //
+    // You MAY modify this function, but the intended functionality must
+    // still work properly.
+
+    int count = 0;
+
+    // Count all the taken seats
+    for (int i = 0; i < seat_count; i++)
+        if (seat_taken[i])
+            count++;
+
+    // Return true if it's the same as seat_taken_count
+    return count == seat_taken_count;
+}
+
+// ------------------- DO NOT MODIFY PAST THIS LINE -------------------
+
+void *seat_broker(void *arg)
+{
+    int *id = arg;
+
+    for (int i = 0; i < transaction_count; i++) {
+        int seat = rand() % seat_count;
+        if (rand() & 1) {
+            // buy a random seat
+            reserve_seat(seat);
+
+        } else {
+            // sell a random seat
+            free_seat(seat);
+        }
+
+        if (!verify_seat_count()) {
+            printf("Broker %d: the seat count seems to be off! " \
+                   "I quit!\n", *id);
+            return NULL;
+        }
+    }
+
+    printf("Broker %d: That all seemed to work very well.\n", *id);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    // Parse command line
+    if (argc != 4) {
+        fprintf(stderr, "usage: reservations seat_count broker_count xaction_count\n");
+        exit(1);
+    }
+
+    seat_count = atoi(argv[1]);
+    broker_count = atoi(argv[2]);
+    transaction_count = atoi(argv[3]);
+
+    // Allocate the seat-taken array
+    seat_taken = calloc(seat_count, sizeof *seat_taken);
+
+    // Allocate thread handle array for all brokers
+    pthread_t *thread = calloc(broker_count, sizeof *thread);
+
+    // Allocate thread ID array for all brokers
+    int *thread_id = calloc(broker_count, sizeof *thread_id);
+
+    srand(time(NULL) + getpid());
+    
+    // Launch all brokers
+    for (int i = 0; i < broker_count; i++) {
+        thread_id[i] = i;
+        pthread_create(thread + i, NULL, seat_broker, thread_id + i);
+    }
+
+    // Wait for all brokers to complete
+    for (int i = 0; i < broker_count; i++)
+        pthread_join(thread[i], NULL);
+}
+
