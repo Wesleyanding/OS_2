ee4a3cec12e9d21df8d4b63275b130acf51c2601
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2024-05-04 21:29:43 -0700

    updated block, added some tests, fixed makefile

--- project5/block.c
+++ project5/block.c
@@ -5,11 +5,19 @@
 int BLOCK_SIZE = 4096; // 4KB
 
 unsigned char *bread(int block_num, unsigned char *block) {
+    int fd = open("disk_image", O_RDONLY);
     
+    lseek(fd, block_num * BLOCK_SIZE, SEEK_SET);
+    read(fd, block, BLOCK_SIZE);
+    close(fd);
+    return block;
 }
 
 void bwrite(int block_num, unsigned char *block) {
-    lseek(block, block_num * BLOCK_SIZE, SEEK_SET);
-    
+    int fd = open("disk_image", O_WRONLY);
+
+    lseek(fd, block_num * BLOCK_SIZE, SEEK_SET);
+    write(fd, block, BLOCK_SIZE);
+    close(fd);
 }
 
