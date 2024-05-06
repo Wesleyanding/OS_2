8e410083f28d317d33474b17225bbad2f0f5dd0e
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2024-05-06 10:45:08 -0700

    start of project 6

---
+++ project6/testfs.c
@@ -0,0 +1,23 @@
+#define CTEST_ENABLE
+#include <stdio.h>
+#include <unistd.h>
+#include "image.h"
+#include "block.h"
+#include "ctest.h"
+
+int main()
+{
+    int result;
+
+    // Test image_open
+    CTEST_VERBOSE(1);
+    result = image_open("disk_image", 1);
+    CTEST_ASSERT(result != -1, "image_open");
+
+    // Test image_close
+    result = image_close();
+    CTEST_ASSERT(result != -1, "image_close");
+
+    CTEST_RESULTS();
+    CTEST_EXIT();
+}
