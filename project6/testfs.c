aa4db9e5ef0fb1b7d4a6312ba6e642f408bfcb12
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2024-05-06 21:57:54 -0700

    added alloc to block.h, added changes to makefile. updated testfs

--- project6/testfs.c
+++ project6/testfs.c
@@ -4,11 +4,13 @@
 #include "image.h"
 #include "block.h"
 #include "ctest.h"
+#include "free.h"
+#include "inode.h"
 
 int main()
 {
     int result;
-
+    // image.c tests
     // Test image_open
     CTEST_VERBOSE(1);
     result = image_open("disk_image", 1);
@@ -17,6 +19,26 @@ int main()
     // Test image_close
     result = image_close();
     CTEST_ASSERT(result != -1, "image_close");
+
+    // block.c tests
+    // Test bread
+
+    // Test bwrite
+
+    // Test alloc
+
+    // inode.c tests
+    // Test ialloc
+
+
+    // free.c tests
+    // Test set_free
+
+    // Test find_low_clear_bit
+
+    // Test find_free
+
+    
 
     CTEST_RESULTS();
     CTEST_EXIT();
