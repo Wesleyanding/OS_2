57ef72c0e6b9e3657f998313d2df956fe9abb7a3
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2024-05-26 20:57:15 -0700

    moved struct up to fix issue

--- project8/dir.h
+++ project8/dir.h
@@ -4,12 +4,6 @@
 
 struct directory *directory_open(int inode_num);
 
-int directory_get(struct directory *dir, struct directory_entry *ent);
-
-void directory_close(struct directory *dir);
-
-void mkfs(void);
-
 struct directory {
     struct inode *inode;
     unsigned int offset;
@@ -19,4 +13,11 @@ struct directory_entry {
     unsigned int inode_num;
     char name[16];
 };
+
+int directory_get(struct directory *dir, struct directory_entry *ent);
+
+void directory_close(struct directory *dir);
+
+void mkfs(void);
+
 #endif 
